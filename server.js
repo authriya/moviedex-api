4d5f711feb1868c06740bd95f5738ed9fee09651
Supriya Ganesh <supriya@thinkful.com>
2020-01-15 13:48:13 -0500

    first draft

--- server.js
+++ server.js
@@ -1,12 +1,50 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const helmet = require('helmet')
+const cors = require('cors')
+const MOVIES = require('./movies-data-small.json')
 
 const app = express()
 
 app.use(morgan('dev'))
+app.use(helmet())
+app.use(cors())
 
-app.use((req, res) => {
-  res.send('Hello, world!')
+
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+
+    if(!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request'})
+    }
+
+    next()
+})
+
+app.get('/movie', function handleGetMovies(req, res) {
+    let response = MOVIES
+    if (req.query.genre) {
+        response = response.filter(movie =>
+            movie.genre.toLowerCase().includes(req.query.genre.toLowerCase()))
+        if (response.length === 0) {
+            return 'This genre is not in our system. Please try again'
+        }
+    }
+    if(req.query.country) {
+        response = response.filter(movie =>
+            movie.country.toLowerCase().includes(req.query.country.toLowerCase()))
+        if (response.length === 0) {
+            return 'Movies from this country are not in our system. Please try again'
+        }
+    }
+    if(req.query.avg_vote) {
+        response = response.filter(movie =>
+            Number(movie.avg_vote) >=  Number(req.query.avg_vote))
+    }
+
+    res.json(response)
 })
 
 const PORT = 8000
